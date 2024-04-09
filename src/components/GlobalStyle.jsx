70767fd0e1492d6f1f1fd117c4da961ef37774be
Potyomko Veronika <vnpotomko@gmail.com>
2024-04-09 19:38:07 +0300

    add basic styles

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from "styled-components"
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-left: auto;
+    margin-right: auto;
+
+    @media screen and (max-width: 320px) {
+        width: 320px;
+         margin-top: 17px;
+        margin-bottom: 17px;
+    }
+
+    @media screen and (min-width: 768px) {
+        width: 768px;
+         margin-top: 25px;
+    margin-bottom: 25px;
+    }
+
+    @media screen and (min-width: 1200px) {
+        width: 1200px;
+         margin-top: 40px;
+    margin-bottom: 40px;
+    }
+`
+
+export const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    font-family: 'Roboto', 'Montserrat';
+      font-weights: 400;
+      font-style: normal;
+      line-height: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    padding: 20px;
+  }
+  
+  code {
+    font-family: 'Roboto', 'Montserrat';
+  }
+  
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+  
+  ul {
+    list-style: none;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+  
+  h1,
+  h2,
+  h3,
+  p,
+  ul {
+    margin: 0;
+    padding: 0;
+  }`
---
+++ src/components/Theme.jsx
@@ -0,0 +1,45 @@
+export const Theme = {
+    colors: {
+        black: '#000000',
+        red: '#E7192E',
+        green: '#407946',
+        veryDarkBlue: 'linear-gradient(117.84deg, #1D346A 2.84%, #031634 67.28%)',
+        white: '#fff',
+        whiteBackground: '#E0E5EB',
+        greyHeading: '#F5F6FB',
+        greyBackground:' #F2F5FC',
+        darkOrange: '#FF751D',
+        lightOrange: '#FFDAC0',
+        grey: '#52555F',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    password: 'Montserrat',
+    body: 'Roboto',
+  },
+  fontSizes: {
+    xxs: '10px',
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '34px',
+  },
+    fontWeights: {
+      extraLight: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      bold: 600,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+};
