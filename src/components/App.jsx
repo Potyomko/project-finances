3c1f65598fb3d8cf76236c911f6c61e7c6708bc2
Oleh Tsap <olehtsap07@gmail.com>
2024-04-11 23:56:25 +0300

    написання логіки для першого компоненту

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
+import { Calculation } from "./Calculations/Calcultion/Calculation";
+
 export const App = () => {
   return (
     <>
+      <Calculation/>
+
     <div
       style={{
         height: '100vh',
--- src/components/Calculations/Calcultion/Calculation.jsx
+++ src/components/Calculations/Calcultion/Calculation.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux';
+
 export const Calculation = () => {
+
+    const incomes = useSelector((state)=>state.incomes)
+
     return (
         <>
-        
+            <h1>{incomes}</h1>
         </>
     )
 }
