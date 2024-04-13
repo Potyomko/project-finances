03b6b67ccca3305346da1294f40d9e6b2964d49a
Niky-Niky <nik333nik888@gmail.com>
2024-04-13 22:09:55 +0300

    basic balance component

---
+++ src/components/Balance/ConfirmButton/ConfirmButton.jsx
@@ -0,0 +1,7 @@
+import { Button } from "./ConfirmButton.styled";
+
+export const ConfirmButton = () => {
+
+    return <Button type='button'>ПІДТВЕРДИТИ</Button>
+
+}
---
+++ src/components/Balance/ConfirmButton/ConfirmButton.styled.jsx
@@ -0,0 +1,14 @@
+import {styled} from "styled-components";
+
+export const Button = styled.button`
+
+    color: #52555F;
+    opacity: 0.8;
+    background-color: #F2F5FC;
+    width: 125px;
+    height: 44px;
+    border-radius: 16px;
+    border: 2px solid #FFFFFF;
+    font-family: Roboto;
+    
+`
