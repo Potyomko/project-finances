0b3dc8c109d2c4dc96b920c01597e5bd7872dad9
Oleh Tsap <olehtsap07@gmail.com>
2024-04-23 19:24:25 +0300

    вернув все на місце

--- src/redux/spendings/slice.js
+++ src/redux/spendings/slice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { addSpending, deleteSpending, getSpendings } from "./operations";
+import { logout } from "../../redux/auth/operations";
 
 const slice = createSlice({
     name: 'spendingsSlice',
@@ -6,6 +8,53 @@ const slice = createSlice({
         spendings: [],
         error: null,
         isLoading: false,
+    },
+    extraReducers(builder){
+        builder
+            .addCase(getSpendings.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(getSpendings.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.spendings = action.payload.spendings
+            })
+            .addCase(getSpendings.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error;
+            })
+            .addCase(addSpending.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(addSpending.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.spendings = action.payload.spendings
+            })
+            .addCase(addSpending.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error;
+            })
+            .addCase(deleteSpending.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(deleteSpending.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.spendings = action.payload.spendings
+            })
+            .addCase(deleteSpending.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error;
+            })
+            .addCase(logout.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.spendings = []
+            })
     }
 })
 
