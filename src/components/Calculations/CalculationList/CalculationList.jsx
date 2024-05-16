754f0deb1875d535d7481b51b9a7bfab73314726
Oleh Tsap <oleh.craftman@gmail.com>
2024-05-16 20:35:52 +0300

    стилізації і логінізація 2 компоненту

--- src/components/Calculations/CalculationList/CalculationList.jsx
+++ src/components/Calculations/CalculationList/CalculationList.jsx
@@ -1,25 +1,94 @@
 import { useState } from "react";
-import { LeftArrow } from "./CalculationList.styled";
+import { LeftArrow, List, ListChild, ListDiv } from "./CalculationList.styled";
+import { ReactComponent as Products } from "../../../images/svg/Group 61.svg";
+import { ReactComponent as Cocktail } from "../../../images/svg/cocktail 1.svg";
+import { ReactComponent as Kite } from "../../../images/svg/kite 1.svg";
+import { ReactComponent as Health } from "../../../images/svg/hands-holding-heart 1.svg";
+import { ReactComponent as Car } from "../../../images/svg/car 1.svg";
+import { ReactComponent as Couch } from "../../../images/svg/couch 1.svg";
+import { ReactComponent as Tools } from "../../../images/svg/tools 1.svg";
+import { ReactComponent as Bills } from "../../../images/svg/invoice 1.svg";
+import { ReactComponent as Sport } from "../../../images/svg/clay 1.svg";
+import { ReactComponent as Book } from "../../../images/svg/book 1.svg";
+import { ReactComponent as Ufo } from "../../../images/svg/ufo 1.svg";
 
 export const CalculationList = () => {
 
     const [text, setText] = useState(0);
 
     const handleChangOnInc = (e) => {
-        setText(text === 1);
+        setText(text + 1);
     }
     
     const handleChangOnSpend = (e) => {
-        setText(text === 0);
+        setText(text - 1);
     }
 
     return (
         <>
-            <div>
+            <ListDiv>
                 <LeftArrow type="submit" onClick={handleChangOnInc}></LeftArrow>
                 {text === 0 ? 'Витрати' : 'Дохід'}
                 <button type="submit" onClick={handleChangOnSpend}></button>
-            </div>
+
+                <List>
+                    <ListChild>
+                        <p></p>
+                        <Products/>
+                        <p>Продукти</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Cocktail/>
+                        <p>Алкоголь</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Kite/>
+                        <p>Розваги</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Health/>
+                        <p>Здоров'я</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Car/>
+                        <p>Транспорт</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Couch/>
+                        <p>Все для дому</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Tools/>
+                        <p>Техніка</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Bills/>
+                        <p>Комуналка, зв'язок</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Sport/>
+                        <p>Спорт, хобі</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Book/>
+                        <p>Навчання</p>
+                    </ListChild>
+                    <ListChild>
+                        <p></p>
+                        <Ufo/>
+                        <p>Інше</p>
+                    </ListChild>
+                </List>
+            </ListDiv>
         </>
     )
 }
--- src/components/Calculations/CalculationList/CalculationList.styled.jsx
+++ src/components/Calculations/CalculationList/CalculationList.styled.jsx
@@ -5,3 +5,39 @@ export const LeftArrow = styled.button`
 
 
 `
+export const List = styled.ul`
+display: flex;
+
+gap: 20px;
+
+flex-wrap: wrap;
+
+list-style-type: none;
+`
+export const ListChild = styled.li`
+    margin-left: 228px;
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    &:nth-child(6n+1) {
+    clear: both;
+    margin-right: 0;
+    }
+    &:nth-child(n+7) {
+    margin-top: 45px;
+    }
+`
+export const ListDiv = styled.div`
+
+width: 1060px;
+// height: 358px;
+// left: 110px;
+// top: 250px;
+
+background: #FFFFFF;
+
+box-shadow: 0px 10px 60px rgba(170, 178, 197, 0.2);
+border-radius: 30px;
+`
