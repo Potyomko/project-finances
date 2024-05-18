77c5e447e6a2236fc2fb66797b06afac3533609e
Vlad <vladikhoncharuk@gmail.com>
2024-05-18 19:01:20 +0300

    завершив адаптивку і підправив баланс

--- src/components/Balance/BalanceCounter/BalanceCounterBox.styled.jsx
+++ src/components/Balance/BalanceCounter/BalanceCounterBox.styled.jsx
@@ -2,13 +2,24 @@ import { styled } from "styled-components";
 
 export const Box = styled.div`
 
-    border-radius: 16px;
+    width: 125px;
+    height: 44px;
     border: 2px solid #FFFFFF;
-    width: 121px;
-    height: 40px;
-    background-color: #F2F5FC;
+    border-top-left-radius: 22px;
+    border-bottom-left-radius: 22px;
     display: flex;
     justify-content: center;
     align-items: center;
+
+    @media(min-width: 768px){
+        margin-right: 15px;
+        border-radius: 16px;
+        width: 121px;
+        height: 40px;
+        background-color: #F2F5FC;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
 `
--- src/components/Balance/ConfirmButton/ConfirmButton.styled.jsx
+++ src/components/Balance/ConfirmButton/ConfirmButton.styled.jsx
@@ -7,8 +7,20 @@ export const Button = styled.button`
     background-color: #F2F5FC;
     width: 125px;
     height: 44px;
-    border-radius: 16px;
     border: 2px solid #FFFFFF;
     font-family: Roboto;
+    border-top-right-radius: 22px;
+    border-bottom-right-radius: 22px;
+
+    &:hover{
+        background: #FF751D;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    @media(min-width: 768px){
+        width: 121px;
+        border-radius: 16px;
+    }
     
 `
